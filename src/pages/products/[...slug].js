8b746528e7844d4d27a4b18ca971f1b86ba34633
saadi001 <sheikhsadi1620@gmail.com>
2023-07-27 00:52:48 +0600

    catch all routes explored

---
+++ src/pages/products/[...slug].js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const CatchAllRoutes = () => {
+    return (
+        <div>
+            <h1>This is catch all routes page example</h1>
+        </div>
+    );
+};
+
+export default CatchAllRoutes;
